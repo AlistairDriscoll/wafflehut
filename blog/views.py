bdbbdfe8307a82070b7e090c5ddabe643d094171
Alistair Driscoll <pallythecoder@gmail.com>
2024-09-20 14:22:59 +0000

    Add: Functionality to display four random blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+import random
 
 # Create your views here.
 
-class index(generic.ListView):
-    queryset = Post.objects.all()
+class index(generic.ListView): 
+    posts = Post.objects.all()
+    
+    def get_posts(posts):
+        """
+        Function to return four random posts to be displayed on the home page
+        """
+        post_list = []
+        for p in posts:
+            post_list.append(p)
+        display_posts = random.sample(post_list, 4)
+        
+        return display_posts
+
+    
+    queryset = get_posts(posts)
     template_name = "blog/posts.html"
