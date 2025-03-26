034c1c3f7e25a3b68012360278bbc1eac84353e2
Alistair Driscoll <pallythecoder@gmail.com>
2025-03-26 11:44:56 +0000

    JS only tries to display wafflerank when on the right page

--- static/js/script.js
+++ static/js/script.js
@@ -3,22 +3,29 @@
 
 
 let wafflerankHeader = document.getElementById('wafflerank-header');
-let userScore = wafflerankHeader.getAttribute('data-user_score');
-let waffleRank = "";
 
-if (userScore == 0) {
-    waffleRank = "Yet to waffle!";
-} else if (userScore >= 30) {
-    waffleRank = "Certified Pro Waffler";
-} else if (userScore >= 20) {
-    waffleRank = "Proper waffler!";
-} else if (userScore >= 10) {
-    waffleRank = "Has waffle.";
-} else if (userScore < 10) {
-    waffleRank = "Entry level waffler...";
+/** Checks to see there is a wafflerank-header
+ * If so, calculates what to display as the waffle rank
+ * and displays accordingly
+ */
+if (wafflerankHeader) {
+    let userScore = wafflerankHeader.getAttribute('data-user_score');
+    let waffleRank = "";
+
+    if (userScore == 0) {
+        waffleRank = "Yet to waffle!";
+    } else if (userScore >= 30) {
+        waffleRank = "Certified Pro Waffler";
+    } else if (userScore >= 20) {
+        waffleRank = "Proper waffler!";
+    } else if (userScore >= 10) {
+        waffleRank = "Has waffle.";
+    } else {
+        waffleRank = "Entry level waffler...";
+    }
+
+    wafflerankHeader.innerText = "User Rank: " + waffleRank;
 }
-
-wafflerankHeader.innerText = "User Rank: " + waffleRank;
 
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
